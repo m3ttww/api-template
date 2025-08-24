275e4d74507cadf5e0ddc648af7f9126e30bdd0a
LateAFQ <ssergeenko821@gmail.com>
2025-08-24 17:36:04 +0300

    feat: domain user validators, post_init validators and password validator from the outside for cases when plain_data has changed

--- template/domain/entities/user.py
+++ template/domain/entities/user.py
@@ -1,7 +1,49 @@
-from template.domain.entities.base import Entity
-from template.domain.entities.mixins.with_id import WithId
+from __future__ import annotations
+
+import re
+from typing import Final
+
+from template.domain.entities.base import Entity, entity
+
+MIN_LOGIN_LENGTH: Final[int] = 6
+MAX_LOGIN_LENGTH: Final[int] = 32
+
+MIN_PASSWORD_LENGTH: Final[int] = 8
+MAX_PASSWORD_LENGTH: Final[int] = 20
+SECURE_REGEX: Final[re.Pattern[str]] = re.compile(
+    r"^(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>?])(?=.*[0-9])(?=.*[a-z]).*$"
+)
 
 
-class User(WithId, Entity):
+@entity
+class User(Entity):
     login: str
     password_hash: str
+
+    def __post_init__(self) -> None:
+        self._validate_login(self.login)
+
+    def _validate_login(self, login: str) -> None:
+        if not login:
+            raise ValueError("Login is required")
+        if len(login) < MIN_LOGIN_LENGTH:
+            raise ValueError(
+                f"Login must be at least {MIN_LOGIN_LENGTH} characters long"
+            )
+        if len(login) > MAX_LOGIN_LENGTH:
+            raise ValueError(
+                f"Login must be less than {MAX_LOGIN_LENGTH} characters long"
+            )
+        if not login.isalnum():
+            raise ValueError("Login must be alphanumeric")
+
+    def validate_plain_password(self, password: str) -> None:
+        if len(password) < MIN_PASSWORD_LENGTH:
+            raise ValueError(f"must be at least {MIN_PASSWORD_LENGTH} characters long.")
+        if len(password) > MAX_PASSWORD_LENGTH:
+            raise ValueError(f"must be at most {MAX_PASSWORD_LENGTH} characters long.")
+        if re.match(SECURE_REGEX, password) is None:
+            raise ValueError(
+                "must contain at least one uppercase letter, one"
+                "lowercase letter, one number, and one special character.",
+            )

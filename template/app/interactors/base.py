d0a602fba094ecbf4291fc9b7b2eca5b378269a3
LateAFQ <ssergeenko821@gmail.com>
2025-08-31 21:08:01 +0300

    fix: _handle renamed to _execute

--- template/app/interactors/base.py
+++ template/app/interactors/base.py
@@ -10,8 +10,8 @@ class Command(DTO): ...
 
 class Interactor[C: Command, R: DTO | Entity | None](Protocol):
     async def __call__(self, cmd: C) -> R:
-        return await self._handle(cmd)
+        return await self._execute(cmd)
 
     @abstractmethod
-    async def _handle(self, cmd: C, /) -> R:
+    async def _execute(self, cmd: C, /) -> R:
         raise NotImplementedError

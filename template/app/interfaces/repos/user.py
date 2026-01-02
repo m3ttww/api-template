c64519cf71a17f239e54651c9556883f4fa0f129
m3ttww <ssergeenko821@gmail.com>
2026-01-02 00:15:20 -0500

    feat: implement user login conflict handling and repository method for user retrieval

--- template/app/interfaces/repos/user.py
+++ template/app/interfaces/repos/user.py
@@ -1,6 +1,10 @@
+from abc import abstractmethod
+
+from template.app.interfaces.option import Option
 from template.app.interfaces.repos.crud import CRUDRepo
 from template.domain.entities.user import User
 
 
 class UserRepo(CRUDRepo[User]):
-    pass
+    @abstractmethod
+    async def get_by_login(self, login: str) -> Option[User]: ...
--- template/infra/persistence/psql/repos/user.py
+++ template/infra/persistence/psql/repos/user.py
@@ -1,7 +1,15 @@
+from sqlalchemy import select
+
+from template.app.interfaces.option import Option
 from template.domain.entities.user import User
+from template.infra.option import Option as OptionImpl
 from template.infra.persistence.psql.repos.crud import CRUDRepoImpl
+from template.infra.persistence.psql.tables import user_table
 from template.app.interfaces.repos.user import UserRepo
 
 
 class UserRepoImpl(UserRepo, CRUDRepoImpl[User]):
-    pass
+    async def get_by_login(self, login: str) -> Option[User]:
+        stmt = select(User).where(user_table.c.login == login)
+        result = await self._session.execute(stmt)
+        return OptionImpl(result.scalar_one_or_none())
